edb122b15c59a1fd1d39b47cd8102c25bbfb157d
tanaselearoxana <tanaselearoxana@yahoo.com>
2016-12-05 15:35:27 +0200

    eliminated onUpgrade() implementation method from DataBaseHelper

--- app/src/main/java/com/deveddy/clujbike/data/repository/DatabaseHelper.java
+++ app/src/main/java/com/deveddy/clujbike/data/repository/DatabaseHelper.java
@@ -7,17 +7,15 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(Context context) {
-        super(context, SqlDataBaseContract.DATA_BASE_NAME, null, SqlDataBaseContract.DATA_BASE_VERSION);
+        super(context, SqlDatabaseContract.DATA_BASE_NAME, null, SqlDatabaseContract.DATA_BASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(SqlDataBaseContract.Station.CREATE_TABLE);
+        db.execSQL(SqlDatabaseContract.Station.CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(SqlDataBaseContract.Station.DROP_TABLE);
-        onCreate(db);
     }
 }
