61b2c419a649777cf6d04e041b20845267b1ba32
tanaselearoxana <tanaselearoxana@yahoo.com>
2016-12-05 15:29:26 +0200

    edited getStations() call changed endpoint changed http method (from GET to POST)

--- app/src/main/java/com/deveddy/clujbike/data/api/ClujBikeService.java
+++ app/src/main/java/com/deveddy/clujbike/data/api/ClujBikeService.java
@@ -2,11 +2,13 @@ package com.deveddy.clujbike.data.api;
 
 import com.deveddy.clujbike.data.api.model.StationReadResponse;
 
-import retrofit2.http.GET;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 import rx.Observable;
 
 public interface ClujBikeService {
 
-    @GET("StationApiEntity/Read")
+    @FormUrlEncoded
+    @POST("Station/Read")
     Observable<StationReadResponse> getStations();
 }
