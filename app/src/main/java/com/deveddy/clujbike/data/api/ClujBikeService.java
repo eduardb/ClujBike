e7925d9b8474edb766fffdbada9d7faef0636b5c
tanaselearoxana <tanaselearoxana@yahoo.com>
2016-12-06 18:26:07 +0200

    erase service method encoding

--- app/src/main/java/com/deveddy/clujbike/data/api/ClujBikeService.java
+++ app/src/main/java/com/deveddy/clujbike/data/api/ClujBikeService.java
@@ -2,13 +2,11 @@ package com.deveddy.clujbike.data.api;
 
 import com.deveddy.clujbike.data.api.model.StationReadResponse;
 
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 import rx.Observable;
 
 public interface ClujBikeService {
 
-    @FormUrlEncoded
     @POST("Station/Read")
     Observable<StationReadResponse> getStations();
 }
